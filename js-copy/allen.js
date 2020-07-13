397b0012fbd3d5418d097afa89946e74b7b4245a
Allen <allen@erealmsoft.com>
2020-07-13 18:29:42 +0800

    allen

--- js-copy/allen.js
+++ js-copy/allen.js
@@ -4,8 +4,25 @@
     console.log(a === b);
     a[0] = 11;
     console.log(a,b);
-    // 深拷贝
+    // 深拷贝1
     var c = [0,1,2,3,4];
     var d=JSON.parse(JSON.stringify(c));
     c[0] = 11;
     console.log(c,d)
+    // 深拷贝2
+    var obj1 = {
+        a:{
+            b:1
+        },
+        c:1
+    };
+    var obj2 = {};
+    obj2.a = {};
+    obj2.c = obj1.c;
+    obj2.a.b = obj1.a.b;
+    console.log(obj1); //{a:{b:1},c:1};
+    console.log(obj2); //{a:{b:1},c:1};
+    obj2.a.b = 2;
+    console.log(obj1); //{a:{b:2},c:1};
+    console.log(obj2); //{a:{b:1},c:1};
+
